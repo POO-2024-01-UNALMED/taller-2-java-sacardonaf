009476836549d4645a6659bf5588b5a646fa78ce
sacardonaf <sacardonaf@unal.edu.co>
2024-02-19 17:17:23 -0500

    entrega taller 2 Java
    se realiza el commit con la actividad propuesta como taller 2 de Java

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,34 @@
+package test;
+
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento [] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int numeroAsientos=0;
+		for (int i=0;i<asientos.length; ++i) {
+			if(asientos[i]!=null) {
+				++numeroAsientos;
+			}
+		}return numeroAsientos;
+	}
+	String verificarIntegridad() {
+		if(this.registro==this.motor.registro) {
+			for (int j=0; j<asientos.length;j++) {
+				if (asientos[j]!=null) {
+					if(this.registro!=this.asientos[j].registro) {
+						return("Las piezas no son originales");
+					}
+				}
+			}return("Auto original");
+		}
+		else {
+			return("Las piezas no son originales");
+		}
+	}
+}
